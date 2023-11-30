1446a2120d5055f3796e5a8a8fc3f11ce71968be
Fab-Pineuh <fabien.pineau85@orange.fr>
2023-11-30 01:06:39 +0100

    MAJ

--- scripts/blackstar_handle-race-selection.js
+++ scripts/blackstar_handle-race-selection.js
@@ -1,4 +1,4 @@
-// handle-race-selection.js
+// blackstar_handle-race-selection.js
 document.getElementById('race-select').addEventListener('change', function(event) {
     const race = event.target.value;
     // Ici, vous pouvez ajouter la logique pour afficher la fiche de personnage spécifique à la race
